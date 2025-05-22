35092d57c538d3778a82fc43001dbd572621f2d4
Luismizz <luismiguelsmorais812@gmail.com>
2025-05-22 17:42:08 -0300

    Add files via upload

---
+++ atividade2.py
@@ -0,0 +1,10 @@
+# soma de 2 fluxograma
+# 4 funcções basicas
+
+n1 = float (input("Digite o primeiro numero"))
+n2 = float (input("Digite o segundo numero"))
+
+print ("Resultado da soma: ", n1 + n2)
+print ("Resultado da subtração: ", n1 - n2)
+print ("Resultado da Multiplicação: ", n1 * n2)
+print ("Resultado da Divisão: ", n1 / n2)
