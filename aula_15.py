26f00fd7cdcd451cf98dfb8750bc5e323df88b38
Luismizz <luismiguelsmorais812@gmail.com>
2025-06-11 17:47:55 -0300

    Add files via upload

---
+++ aula_15.py
@@ -0,0 +1,53 @@
+# #retangulo
+
+# import pygame
+# pygame.init()
+# tela  = pygame.display.set_mode((300,300))
+
+        
+# run = True         
+# while run:
+#     for event in pygame.event.get():
+#         if event.type == pygame.QUIT:
+#             run  = False
+#             pygame.quit()
+#     tela.fill('green') 
+#     pygame.draw.rect(tela,('red'),(120,120,70,70))
+#     pygame.display.flip()
+
+# # circulo
+
+# import pygame
+# pygame.init()
+# tela  = pygame.display.set_mode((450,280))
+
+        
+# run = True         
+# while run:
+#     for event in pygame.event.get():
+#         if event.type == pygame.QUIT:
+#             run  = False
+#             pygame.quit()
+#     tela.fill('black') 
+#     pygame.draw.circle(tela,('red'),(200,120),(90))
+#     pygame.display.flip()
+
+# # elipse
+
+# import pygame
+# pygame.init()
+# tela  = pygame.display.set_mode((450,280))
+
+        
+# run = True         
+# while run:
+#     for event in pygame.event.get():
+#         if event.type == pygame.QUIT:
+#             run  = False
+#             pygame.quit()
+#     tela.fill('black') 
+#     pygame.draw.ellipse(tela,('red'),(180,101,181,150))
+#     pygame.display.flip()
+
+      
+                   
